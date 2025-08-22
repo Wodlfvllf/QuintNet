2b5f3a422bbcee7200d6b3a9c33f27c0a11339e0
Wodlfvllf <shuklashashankshekhar863@gmail.com>
2025-08-22 18:11:55 +0000

    Process group for tensor parallelism

--- TensorParallelism/processgroup.py
+++ TensorParallelism/processgroup.py
@@ -1,4 +1,14 @@
 import torch
 import torch.nn as nn
+import torch.distributed as dist
 
-class Proc
+class ProcessGroupManager:
+    def __init__(self, tp_size):
+        world_size = dist.get_world_size()
+        rank = dist.get_rank()
+        
+        assert world_size == tp_size, "world_size must be Equivalent to tp_size"
+        
+        # For pure TP: just make one group of all ranks
+        tp_group = dist.new_group(ranks=list(range(world_size)))
+        return tp_group
