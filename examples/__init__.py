054a4b913cb14d5d5bdd8bc87fd3585fe3860482
Shashank Shekhar Shukla <shuklashashankshekhar863@gmail.com>
2025-11-06 21:53:03 +0530

    init

---
+++ examples/__init__.py
@@ -0,0 +1,11 @@
+"""
+Example Scripts for QuintNet.
+
+This directory contains simple examples demonstrating:
+- Data Parallelism only
+- Tensor Parallelism only
+- Pipeline Parallelism only
+- Full 3D Parallelism
+"""
+
+__all__ = []
