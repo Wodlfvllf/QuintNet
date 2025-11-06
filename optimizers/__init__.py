7a182a3a686f3ec900b5b881af4dde81ac71fa62
Shashank Shekhar Shukla <shuklashashankshekhar863@gmail.com>
2025-11-06 23:14:19 +0530

    init

---
+++ optimizers/__init__.py
@@ -0,0 +1,12 @@
+"""
+Distributed-Aware Optimizers.
+
+Future implementations:
+- ZeRO optimizer (ZeRO-1, ZeRO-2, ZeRO-3)
+- Distributed AdamW
+- Fused optimizers
+"""
+
+# TODO: Implement distributed optimizers
+
+__all__ = []
