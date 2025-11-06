495a164e805dc8a1d251f1c8b3b665761b479eb1
Shashank Shekhar Shukla <shuklashashankshekhar863@gmail.com>
2025-11-06 23:13:58 +0530

    pipeline parallelism

---
+++ examples/simple_pp.py
@@ -0,0 +1,8 @@
+"""
+Simple Pipeline Parallelism Example.
+
+TODO: Create minimal example showing how to use PipelineParallelWrapper
+"""
+
+# Example usage:
+# torchrun --nproc_per_node=4 simple_pp.py
