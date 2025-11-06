ce295eeebc70c472539b760f92437d1d18076660
Shashank Shekhar Shukla <shuklashashankshekhar863@gmail.com>
2025-11-07 00:45:10 +0530

    removing older structure

--- DataParallelsim/backends/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Distributed backends for CustomDDP."""
-
-from .base import DistributedBackend
-from .torch_backend import TorchDistributedBackend
-from .local_backend import LocalBackend
-
-__all__ = ["DistributedBackend", "TorchDistributedBackend", "LocalBackend"]
