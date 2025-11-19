b19582b3a3e2c808a75719a75dfea96fdb6d0805
Shashank Shekhar Shukla <shuklashashankshekhar863@gmail.com>
2025-11-19 10:11:38 +0530

    feat: Add DataPipelineParallelStrategy

---
+++ strategy/dp_pp_strategy.py
@@ -0,0 +1,12 @@
+import torch.nn as nn
+from QuintNet.strategy.base_strategy import BaseStrategy
+
+class DataPipelineParallelStrategy(BaseStrategy):
+    """
+    Data and Pipeline Parallelism Strategy (DP+PP).
+    """
+    def apply(self, model: nn.Module) -> nn.Module:
+        from QuintNet.coordinators.dp_pp_coordinator import DPPCoordinator
+        
+        coordinator = DPPCoordinator(model, self.pg_manager, self.config, self.device)
+        return coordinator.parallelize()
