12abd5725ef00a2424651709e76eeada03c55601
Shashank Shekhar Shukla <shuklashashankshekhar863@gmail.com>
2025-11-19 09:49:30 +0530

    feat: Add TensorParallelCoordinator

---
+++ coordinators/tensor_parallel_coordinator.py
@@ -0,0 +1,46 @@
+import torch
+import torch.nn as nn
+import torch.distributed as dist
+from QuintNet.coordinators.main_coordinator import BaseCoordinator
+from QuintNet.core.process_groups import ProcessGroupManager
+from QuintNet.parallelism.tensor_parallel.rewrite import apply_tensor_parallel
+
+class TensorParallelCoordinator(BaseCoordinator):
+    """
+    Coordinator for applying Tensor Parallelism.
+    """
+    def __init__(self, model: nn.Module, pg_manager: ProcessGroupManager, config: dict, device, **kwargs):
+        """
+        Initializes the TensorParallelCoordinator.
+
+        Args:
+            model (nn.Module): The model to be parallelized.
+            pg_manager (ProcessGroupManager): The process group manager.
+            config (dict): The configuration dictionary.
+            device: The device to move the model to.
+        """
+        super().__init__(model, **kwargs)
+        self.pg_manager = pg_manager
+        self.config = config
+        self.device = device
+
+    def parallelize(self) -> nn.Module:
+        """
+        Applies Tensor Parallelism to the.
+
+        Returns:
+            nn.Module: The tensor-parallel model.
+        """
+        global_rank = dist.get_rank()
+        tp_group = self.pg_manager.get_group('tp')
+        coords = self.pg_manager.get_coordinates_tensor_search(global_rank)
+        tp_rank = coords[self.config['mesh_name'].index('tp')]
+
+        self.model.to(self.device)
+        return apply_tensor_parallel(
+            self.model,
+            tp_size=self.config['mesh_dim'][self.config['mesh_name'].index('tp')],
+            tp_rank=tp_rank,
+            tp_group=tp_group,
+            device=self.device
+        )
