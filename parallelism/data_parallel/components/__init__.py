c9bb55a2b451dea0e7bb90124d1ed09352ccb79c
Shashank Shekhar Shukla <shuklashashankshekhar863@gmail.com>
2025-11-26 21:56:55 +0530

    fixed imports

--- parallelism/data_parallel/components/__init__.py
+++ parallelism/data_parallel/components/__init__.py
@@ -4,13 +4,13 @@ Components for Data Parallel Training.
 Migration Source: QuintNet/DataParallelsim/components/
 """
 
-from .bucket import Bucket
+from .bucket import GradientBucket
 from .bucket_manager import BucketManager
 from .gradient_reducer import GradientReducer
 from .parameter_broadcaster import ParameterBroadcaster
 
 __all__ = [
-    'Bucket',
+    'GradientBucket',
     'BucketManager',
     'GradientReducer',
     'ParameterBroadcaster',
