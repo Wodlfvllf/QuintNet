4e42985b367d0bd6793a7614b35103e77b7b3236
Shashank Shekhar Shukla <shuklashashankshekhar863@gmail.com>
2026-01-31 18:22:22 +0530

    corrected init file for exporting gpt2 classes

--- utils/GPT2/__init__.py
+++ utils/GPT2/__init__.py
@@ -2,15 +2,13 @@
 from .gpt2_block import GPT2Block
 from .gpt2_embeddings import GPT2Embedding
 from .gpt2_model import GPT2Model
-from .gpt2_pipeline import GPT2Pipeline
 from .gpt2_stage import GPT2Stage
-from .gpt2_utils import load_gpt2_distributed
+from .gpt2_attention import GPT2Attention
 
 __all__ = [
     "GPT2Block",
     "GPT2Embedding",
     "GPT2Model",
-    "GPT2Pipeline",
     "GPT2Stage",
-    "load_gpt2_distributed",
+    "GPT2Attention",
 ]
