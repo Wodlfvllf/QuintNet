b4c82f963eccd00fe5e6f01aae36897e159f9bcf
Shashank Shekhar Shukla <shuklashashankshekhar863@gmail.com>
2026-01-31 18:39:14 +0530

    modified init file of GPT2 module

--- utils/GPT2/__init__.py
+++ utils/GPT2/__init__.py
@@ -1,14 +1,14 @@
 
 from .gpt2_block import GPT2Block
 from .gpt2_embeddings import GPT2Embedding
-from .gpt2_model import GPT2Model
 from .gpt2_stage import GPT2Stage
 from .gpt2_attention import GPT2Attention
+from .gpt2_mlp import GPT2MLP
 
 __all__ = [
     "GPT2Block",
     "GPT2Embedding",
-    "GPT2Model",
     "GPT2Stage",
     "GPT2Attention",
+    "GPT2MLP",
 ]
